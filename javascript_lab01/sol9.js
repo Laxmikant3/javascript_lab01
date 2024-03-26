c5afada9ff6b2f1e67874faa23708dda0a773bc0
Laxmikant3 <132117545+Laxmikant3@users.noreply.github.com>
2024-03-26 20:32:26 +0530

    Honor Lab-1 Assignment

---
+++ javascript_lab01/sol9.js
@@ -0,0 +1,22 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+function capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1)
+}
+rl.question("Enter a city name: ", function(userIp) {
+    userIp = userIp.trim();
+    let ip = capitalize(userIp)
+    console.log(ip)
+    const cities = ["Nagpur", "Mumbai", "Pune", "Chennai", "Delhi"];
+    const index = cities.indexOf(ip);
+    if (index === -1) {
+      console.log("Not Found");
+    }
+    else {
+      console.log(`Found at ${index} `);
+    }
+    rl.close();
+  });
